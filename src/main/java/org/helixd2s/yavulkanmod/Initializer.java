c75fd04e2342042898ade62c7a76cff43f992619
helixd-2k18 <helixd.2k18@gmail.com>
2022-06-03 22:04:57 +0800

    not possible currently

--- src/main/java/org/helixd2s/yavulkanmod/Initializer.java
+++ src/main/java/org/helixd2s/yavulkanmod/Initializer.java
@@ -5,9 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 public class Initializer implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
     @Override
     public void onInitialize() {
-
+        LOGGER.info("Hello Fabric world!");
     }
 }
